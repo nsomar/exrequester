781c9de68cf6e09d24afa5f89a7ed1cfa7e43a7f
Omar Abdelhafith <o.arrabi@me.com>
2016-02-22 10:34:43 +0000

    Adding coverex to mix.ex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule EXRequester.Mixfile do
     elixir: "~> 1.0",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
+    test_coverage: [tool: Coverex.Task, coveralls: true],
     deps: deps]
   end
 
@@ -17,6 +18,7 @@ defmodule EXRequester.Mixfile do
   defp deps do
     [{:httpotion, "~> 2.2.0"},
     {:poison, "~> 1.5"},
-    {:httpotion, "~> 2.2"}]
+    {:httpotion, "~> 2.2"},
+    {:coverex, "~> 1.4.7", only: :test}]
   end
 end
